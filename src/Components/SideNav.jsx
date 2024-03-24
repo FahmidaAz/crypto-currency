a5ec8873b2a3a468b39a85559da96e04409058e8
FahmidaAz <66080543+FahmidaAz@users.noreply.github.com>
2024-03-23 21:30:29 -0400

    file-commit

---
+++ src/Components/SideNav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SideNav = () => {
+    const SideNav = () => (
+        <div className="sidenav">
+          <h1>here is my side nav bar</h1>
+        </div>
+      );
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SideNav;
